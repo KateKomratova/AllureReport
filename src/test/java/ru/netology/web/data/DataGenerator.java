e8e0f58c6c0f591be14b54c5be8953009007de7c
Ekaterina Komratova <ekaterina.komratova@gmail.com>
2022-02-01 23:18:59 +0300

    Added allure-report

--- src/test/java/ru/netology/web/data/DataGenerator.java
+++ src/test/java/ru/netology/web/data/DataGenerator.java
@@ -1,6 +1,7 @@
 package ru.netology.web.data;
 
 import com.github.javafaker.Faker;
+import io.qameta.allure.Step;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -14,7 +15,9 @@ public class DataGenerator {
 
     static Faker faker = new Faker(new Locale("ru"));
 
+
     public static class Registration {
+        @Step("Генерируем валидные данные для заполнения формы заказа карты: город, ФИО, телефон")
         public static RegistrationInfo generateInfo(String locale) {
             return new RegistrationInfo(generateCity(),
                     generateName(),
@@ -22,6 +25,7 @@ public class DataGenerator {
         }
     }
 
+    @Step("Генерируем город для доставки карты")
     public static String generateCity() {
         String[] city = new String[]
                 {"Москва", "Кемерово", "Майкоп", "Симферополь", "Тамбов", "Мурманск", "Владимир", "Самара", "Магадан"};
@@ -29,16 +33,26 @@ public class DataGenerator {
         return city[index];
     }
 
+    @Step("Генерируем дату для доставки карты")
     public static String generateDate(int plusDays) {
         return LocalDate.now().plusDays(plusDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
+    @Step("Генерируем Ф.И.О. для заказа карты")
     public static String generateName() {
         return faker.name().fullName();
     }
 
+    @Step("Генерируем телефон для связи с курьером")
     public static String generatePhone() {
         return faker.phoneNumber().phoneNumber();
     }
+
+    @Step("Генерируем недопустимое имя для проверки метода")
+    public static String generateInvalidName() {
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().firstName();
+    }
+
 }
 
--- src/test/java/ru/netology/web/test/CardOrderDeliveryDateChangeTest.java
+++ src/test/java/ru/netology/web/test/CardOrderDeliveryDateChangeTest.java
@@ -1,9 +1,11 @@
 package ru.netology.web.test;
 
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
 import lombok.val;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.web.data.DataGenerator;
 
@@ -14,15 +16,28 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
+@Feature("Тестируем функциональность доставки карты")
 public class CardOrderDeliveryDateChangeTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
     @BeforeEach
     public void setUp() {
-        Configuration.headless = true;
+//        Configuration.headless = true;
         open("http://0.0.0.0:9999");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.chord(Keys.CONTROL, "A"), Keys.DELETE);
     }
 
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @Story("Позитивные кейсы: доставка карты оформлена успешно")
+    @DisplayName("Проверяем перепланирование встречи с курьером на другую дату")
     @Test
     public void shouldCardDeliveryOrder() {
         val user = DataGenerator.Registration.generateInfo("ru");
@@ -37,10 +52,75 @@ public class CardOrderDeliveryDateChangeTest {
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.chord(Keys.CONTROL, "A"), Keys.DELETE);
         $("[data-test-id=date] input").val(DataGenerator.generateDate(5));
         $x("//*[text()='Запланировать']").doubleClick();
-        $(withText("У вас уже запланирована встреча на другую дату")).shouldHave(text("Перепланировать?"), Duration.ofSeconds(1500));
+        $(withText("У вас уже запланирована встреча на другую дату")).shouldHave(text("Перепланировать?"), Duration.ofSeconds(20));
         $(byText("Перепланировать")).click();
         $("[data-test-id=success-notification]")
-                .shouldHave(text("Успешно! Встреча успешно запланирована на " + DataGenerator.generateDate(5)), Duration.ofSeconds(1500));
+                .shouldHave(text("Успешно! Встреча успешно запланирована на " + DataGenerator.generateDate(5)), Duration.ofSeconds(15));
 
     }
+
+    @Story("Негативные кейсы: доставка карты не оформлена")
+    @DisplayName("Проверяем отправку формы заказа карты с невалидным именем")
+    @Test
+    public void shouldCardDeliveryOrderInvalidName() {
+        $("[data-test-id=city] input").val(DataGenerator.generateCity());
+        $("[data-test-id=date] input").val(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(DataGenerator.generateInvalidName());
+        $("[data-test-id=phone] input").val(DataGenerator.generatePhone());
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()='Запланировать']").click();
+        $("[data-test-id = 'name'].input_invalid .input__sub")
+                .shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Story("Негативные кейсы: доставка карты не оформлена")
+    @DisplayName("Проверяем отправку формы заказа карты с недопустимой датой встречи")
+    @Test
+    public void shouldCardDeliveryOrderInvalidDate() {
+        $("[data-test-id=city] input").val(DataGenerator.generateCity());
+        $("[data-test-id=date] input").val(DataGenerator.generateDate(1));
+        $("[data-test-id=name] input").setValue(DataGenerator.generateName());
+        $("[data-test-id=phone] input").val(DataGenerator.generatePhone());
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()='Запланировать']").click();
+        $("[data-test-id=date] .input_invalid .input__sub")
+                .shouldHave(text("Заказ на выбранную дату невозможен"));
+    }
+
+    @Story("Негативные кейсы: доставка карты не оформлена")
+    @DisplayName("Проверяем отправку формы заказа карты: поле город не заполнено")
+    @Test
+    public void shouldCardDeliveryOrderEmptyCity() {
+        $("[data-test-id=date] input").val(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(DataGenerator.generateName());
+        $("[data-test-id=phone] input").val(DataGenerator.generatePhone());
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()='Запланировать']").click();
+        $("[data-test-id = 'city'].input_invalid").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Story("Негативные кейсы: доставка карты не оформлена")
+    @DisplayName("Проверяем отправку формы заказа карты: поле имя не заполнено")
+    @Test
+    public void shouldCardDeliveryOrderEmptyName() {
+        $("[data-test-id=city] input").val(DataGenerator.generateCity());
+        $("[data-test-id=date] input").val(DataGenerator.generateDate(3));
+        $("[data-test-id=phone] input").val(DataGenerator.generatePhone());
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()='Запланировать']").click();
+        $("[data-test-id = 'name'].input_invalid").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Story("Негативные кейсы: доставка карты не оформлена")
+    @DisplayName("Проверяем отправку формы заказа карты: поле телефон не заполнено")
+    @Test
+    public void shouldCardDeliveryOrderEmptyPhone() {
+        $("[data-test-id=city] input").val(DataGenerator.generateCity());
+        $("[data-test-id=date] input").val(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(DataGenerator.generateName());
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()='Запланировать']").click();
+        $("[data-test-id = 'phone'].input_invalid").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
 }
